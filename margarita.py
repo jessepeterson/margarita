57e16c92ca4e21f0fc0035c3f77cbb11b72f2d62
Jesse Peterson <jesse.c.peterson@gmail.com>
2012-03-25 03:13:25 -0700

    Initial import of margarita

---
+++ margarita.py
@@ -0,0 +1,123 @@
+from flask import Flask
+from flask import jsonify, render_template, redirect
+from flask import request
+app = Flask(__name__)
+
+import os
+import json
+
+
+from reposadolib import reposadocommon
+
+@app.route('/')
+def index():
+    return render_template('margarita.html')
+
+@app.route('/list_branches', methods=['POST'])
+def list_branches():
+    '''Returns catalog branch names'''
+    catalog_branches = reposadocommon.getCatalogBranches()
+    return jsonify(result=catalog_branches)
+
+@app.route('/products', methods=['POST'])
+def products():
+    products = reposadocommon.getProductInfo()
+    prodlist = []
+    for prodid in products.keys():
+	prodlist.append({
+	    'title':    products[prodid]['title'],
+	    'version':  products[prodid]['version'],
+	    'PostDate': products[prodid]['PostDate'].strftime('%Y-%m-%d'),
+	    'id':       prodid,
+	    })
+    
+    from operator import itemgetter
+    sprodlist = sorted(prodlist, key=itemgetter('PostDate'), reverse=True)
+
+    return jsonify(result=sprodlist)
+
+@app.route('/new_branch/<branchname>', methods=['POST'])
+def new_branch(branchname):
+    catalog_branches = reposadocommon.getCatalogBranches()
+    if branchname in catalog_branches:
+        reposadocommon.print_stderr('Branch %s already exists!', branchname)
+        abort(401)
+    catalog_branches[branchname] = []
+    reposadocommon.writeCatalogBranches(catalog_branches)
+    
+    return jsonify(result='success')
+
+@app.route('/delete_branch/<branchname>', methods=['POST'])
+def delete_branch(branchname):
+    catalog_branches = reposadocommon.getCatalogBranches()
+    if not branchname in catalog_branches:
+        reposadocommon.print_stderr('Branch %s does not exist!', branchname)
+        return
+
+    del catalog_branches[branchname]
+
+    # this is not in the common library, so we have to duplicate code
+    # from repoutil
+    for catalog_URL in reposadocommon.pref('AppleCatalogURLs'):
+        localcatalogpath = reposadocommon.getLocalPathNameFromURL(catalog_URL)
+        # now strip the '.sucatalog' bit from the name
+        if localcatalogpath.endswith('.sucatalog'):
+            localcatalogpath = localcatalogpath[0:-10]
+        branchcatalogpath = localcatalogpath + '_' + branchname + '.sucatalog'
+        if os.path.exists(branchcatalogpath):
+            reposadocommon.print_stdout(
+                'Removing %s', os.path.basename(branchcatalogpath))
+            os.remove(branchcatalogpath)
+
+    reposadocommon.writeCatalogBranches(catalog_branches)
+    
+    return jsonify(result=True);
+
+@app.route('/add_all/<branchname>', methods=['POST'])
+def add_all(branchname):
+	products = reposadocommon.getProductInfo()
+	catalog_branches = reposadocommon.getCatalogBranches()
+	
+	catalog_branches[branchname] = products.keys()
+
+	reposadocommon.writeCatalogBranches(catalog_branches)
+	reposadocommon.writeAllBranchCatalogs()
+	
+	return jsonify(result=True)
+
+
+@app.route('/process_queue', methods=['POST'])
+def process_queue():
+    queue = json.loads(request.form['queue'])
+
+    catalog_branches = reposadocommon.getCatalogBranches()
+    
+    for cat in queue['listing']:
+	if cat not in catalog_branches.keys():
+	    print 'No such catalog'
+	    continue
+	    
+	for prodid in queue['listing'][cat]:
+	    if prodid not in catalog_branches[cat]:
+		# TODO: check for actual prodid?
+		print 'Adding product',prodid,'to cat',cat
+		catalog_branches[cat].append(prodid)
+
+    for cat in queue['delisting']:
+	if cat not in catalog_branches.keys():
+	    print 'No such catalog'
+	    continue
+	    
+	for prodid in queue['delisting'][cat]:
+	    if prodid in catalog_branches[cat]:
+		print 'Removing product',prodid,'from cat',cat
+		catalog_branches[cat].remove(prodid)
+
+    reposadocommon.writeCatalogBranches(catalog_branches)
+    reposadocommon.writeAllBranchCatalogs()
+
+    
+    return jsonify(result=True);
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
---
+++ static/js/margarita.js
@@ -0,0 +1,277 @@
+var branches = {}
+var branchct = 0;
+var listing_queue = {"listing": {}, "delisting": {}};
+var hideCommonlyListed = true;
+
+function queue_count() {
+    var totalCt = 0;
+    
+    for (var i in  listing_queue['listing']) {
+	totalCt += listing_queue['listing'][i].length;
+    }
+
+    for (var i in  listing_queue['delisting']) {
+	totalCt += listing_queue['delisting'][i].length;
+    }
+
+    return totalCt;
+}
+
+function reset_queue() {
+    listing_queue = {"listing": {}, "delisting": {}};
+    update_queue_items();
+}
+
+function update_queue_items()
+{
+    var totalCt = queue_count();
+
+    $("span#queueCount").html(totalCt);
+
+    if (totalCt > 0) {
+	$("span#queueCount").attr('class', 'badge badge-info');
+    } else {
+	$("span#queueCount").attr('class', 'badge badge-inverse');
+    }
+}
+
+function delete_branch(branchname) {
+    $.post("/delete_branch/" + branchname, function() {
+	    refresh_updates_table();
+    }, 'json');
+}
+
+function add_all(branchname) {
+    $.post("/add_all/" + branchname, function() {
+	    refresh_updates_table();
+    }, 'json');
+}
+
+
+function branch_header_cell(branchname) {
+    return '<th class="branchcolumn">' +
+	'<i class="icon-list"></i> <span>' + branchname + ' branch</span>' +
+	'</th>';
+}
+
+function refresh_updates_table() {
+
+    reset_queue();
+
+    // remove existing data rows
+    $("#swupdates").empty();
+
+    // retrive list of branches
+    $.post("/list_branches", function(data) {
+	branches = data['result'];
+
+	// remove existing branch table columns
+	$("th.branchcolumn").remove();
+
+	// loop through branches and create columns
+	var text = branch_header_cell('direct');
+	$.each(branches, function(branch) {
+	    text += branch_header_cell(branch);
+	});
+	$("#swupdatecols").append(text);
+
+	// javascript seems to not have a standard way to count
+	// dictionaries, so do it ahead of time
+	branchct = 0;
+	for (var k in branches)
+	    branchct++;
+
+	// we can refresh the rows now
+	refresh_updates_rows();
+    }, 'json');
+    
+}
+
+function hover_unlisted(e, inout) {
+    if (inout == 'in' && $(e).hasClass('queued')) {
+	$("span", e).html('Dequeue listing');
+	$("i", e).attr("class", "icon-minus icon-white");
+    } else if (inout == 'in') {
+	$("span", e).html('Queue listing');
+	$("i", e).attr("class", "icon-plus");
+    } else if (inout == 'out' && $(e).hasClass('queued')) {
+	$("span", e).html('Listing queued');
+	$("i", e).attr("class", "icon-plus icon-white");
+    } else if (inout == 'out') {
+	$("span", e).html('Unlisted');
+	$("i", e).attr("class", "icon-remove");
+    }
+}
+
+function hover_listed(e, inout) {
+    if (inout == 'in' && $(e).hasClass('queued')) {
+	$("span", e).html('Dequeue delisting');
+	$("i", e).attr("class", "icon-minus icon-white");
+    } else if (inout == 'in') {
+	$("span", e).html('Queue delisting');
+	$("i", e).attr("class", "icon-minus icon-white");
+    } else if (inout == 'out' && $(e).hasClass('queued')) {
+	$("span", e).html('Delisting queued');
+	$("i", e).attr("class", "icon-minus icon-white");
+    } else if (inout == 'out') {
+	$("span", e).html('Listed');
+	$("i", e).attr("class", "icon-ok icon-white");
+    }
+}
+
+function toggle_queue_listing(e, prodid, branch) {
+
+    if ($(e).hasClass('listed')) {
+	var listing = 'delisting';
+    } else {
+	var listing = 'listing';
+    }
+
+    if (branch in listing_queue[listing]) {
+	// branch exists, let's toggle the prodid
+
+	var idx = listing_queue[listing][branch].indexOf(prodid);
+	if (idx != -1 ) {
+	    listing_queue[listing][branch].splice(idx, 1);
+
+	    $(e).removeClass('queued');
+
+	    if (listing == 'listing') {
+		$(e).removeClass('btn-info');
+		hover_unlisted(e, 'in');
+	    } else {
+		$(e).removeClass('btn-info');
+		$(e).addClass('btn-success');
+		hover_listed(e, 'in');
+	    }
+	} else {
+	    listing_queue[listing][branch].push(prodid);
+
+	    $(e).addClass('queued');
+	    if (listing == 'listing') {
+		$(e).addClass('btn-info');
+		hover_unlisted(e, 'in');
+	    } else {
+		$(e).removeClass('btn-success');
+		$(e).addClass('btn-info');
+		hover_listed(e, 'in');
+	    }
+	}
+
+    } else {
+	// branch does not exist, create it with initial prodid
+	listing_queue[listing][branch] = [prodid];
+
+	$(e).addClass('queued');
+	if (listing == 'listing') {
+	    $(e).addClass('btn-info');
+	    hover_unlisted(e, 'in');
+	} else {
+	    $(e).removeClass('btn-success');
+	    $(e).addClass('btn-info');
+	    hover_listed(e, 'in');
+	}
+    }
+
+    update_queue_items();
+}
+
+function refresh_updates_rows() {
+    // existing rows should not exist
+
+    $.post("/products", function(products) {
+
+	var allrowtext = '';
+
+	// loop through products and create rows
+	$.each(products['result'], function(row, product) {
+
+	    var text = '';
+	    
+	    var unlistedProducts = false;
+	    
+	    // main informational columns
+	    text += '<tr><td>' +
+		product['title'] + '</td><td>' +
+		product['version'] + '</td><td>' +
+		product['PostDate'] + '</td>';
+
+	    /* create the disabled indicator for the 'raw' catalogs
+	     * this is just for show as we cannot unlist raw catalog
+	     * updates.
+	     */
+	    text += '<td><button disabled class="btn btn-mini btn-primary disabled">' +
+		'<i class="icon-ok icon-white"></i> Listed</button></td>';
+
+	    // loop through branches
+	    $.each(branches, function(branch) {
+		text += '<td>' +
+		    '<button class="btn btn-mini ';
+
+		// search for prodid in catalog product index
+		if (branches[branch].indexOf(product['id']) != -1) {
+		    text += 'listed btn-success" onClick="toggle_queue_listing(this,' + "'" +
+			product['id'] + "','" + 
+			branch + "'" + ');"' +
+			'><i class="icon-ok icon-white"></i> <span>Listed</span></button>';
+			'</td>';
+		} else {
+		    unlistedProducts = true;
+		    text += 'unlisted" onClick="toggle_queue_listing(this,' + "'" +
+			product['id'] + "','" + 
+			branch + "'" + ');"' +
+			'><i class="icon-remove"></i> <span>Unlisted</span></button>';
+			'</td>';
+		}
+		
+	    });
+
+	    text += '</td></tr>';
+	    
+	    // hide commonly listed items to save space
+	    if (unlistedProducts || (hideCommonlyListed == false)) {
+		allrowtext += text;
+	    }
+	});
+	$("#swupdates").append(allrowtext);
+
+	// assign hover events for the new buttons
+	$('button.unlisted').hover(
+	    function(e) { hover_unlisted(this, 'in'); },
+	    function(e) { hover_unlisted(this, 'out'); }
+	);
+	$('button.listed').hover(
+	    function(e) { hover_listed(this, 'in'); },
+	    function(e) { hover_listed(this, 'out'); }
+	);
+
+	if (branchct > 0) {
+		// add all button
+	    text = '<tr><td colspan="4"></td>';
+	    $.each(branches, function(branch) {
+		text += '<td><button class="btn btn-mini btn-info" ' +
+		    'onclick="add_all(' + "'" + branch + "'" + ');">Add all to ' + branch + ' branch</button></td>';
+	    });
+	    $("#swupdates").append(text + '</td>');
+
+		// delete button
+	    text = '<tr><td colspan="4"></td>';
+	    $.each(branches, function(branch) {
+		text += '<td><button class="btn btn-mini btn-danger" ' +
+		    'onclick="delete_branch(' + "'" + branch + "'" + ');">Delete ' + branch + ' branch</button></td>';
+	    });
+	    $("#swupdates").append(text + '</td>');
+	}
+	
+    }, 'json');
+}
+
+function submit_queue() {
+    if ($.active == 0 && queue_count() > 0) {
+		var cached_queue = listing_queue;
+		reset_queue();
+		$.post("/process_queue", {'queue': JSON.stringify(cached_queue)}, function(data) {
+		    refresh_updates_table();
+		});
+    }
+}
