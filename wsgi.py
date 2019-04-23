053d8d3fe83ae0c619781ce7d84edbf1d99cf183
Harry Seeber <harry.seeber@gusto.com>
2019-04-23 16:42:59 -0600

    fix: nginx + uwsgi config

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 sys.path = list(set([p for p in sys.path if p]))
-from margarita.main import app
+from margarita.main import app as application
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
